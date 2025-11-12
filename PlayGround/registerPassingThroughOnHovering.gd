129aa3cba4e55ee26c866ca0ca0bab77ace69f97
Xiaobin <xiaobin@tal.com>
2025-11-12 15:47:58 +0800

    inital commit

---
+++ PlayGround/registerPassingThroughOnHovering.gd
@@ -0,0 +1,13 @@
+extends Node
+
+static var _hoveringCount: int = 0;
+
+func _on_area_2d_mouse_entered() -> void:
+	_hoveringCount += 1;
+	get_window().mouse_passthrough = false;
+	pass
+
+func _on_area_2d_mouse_exit() -> void:
+	_hoveringCount -= 1;
+	get_window().mouse_passthrough = _hoveringCount <= 0;
+	pass
