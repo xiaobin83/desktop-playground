8690b160d197a4ffdee0600befa759a6dd3d046f
xiaobin <xiaobin.huang@gmail.com>
2026-01-06 22:52:48 +0800

    Update project files:
    1. Add godot-rl-agents/logs/ to .gitignore to exclude training logs
    2. Add VS Code launch configuration in .vscode/launch.json
    3. Update simulate_player.gd with network functionality changes
    4. Enhance Readme.md with additional project information

--- PlayGround/network/simulate_player.gd
+++ PlayGround/network/simulate_player.gd
@@ -11,8 +11,13 @@ func _enter_tree() -> void:
 	var args = OS.get_cmdline_args()
 	var dict = Utils.parse_args(args)
 	var simulate_player = dict.get('simulate-player', null)
-	var player = Utils.get_enum_value(SimulatedPlayer, simulate_player)
-	if player != null:
-		_simulate_player = player
+	if simulate_player == null:
+		_simulate_player = SimulatedPlayer.PlayerA
+	else:
+		var player = Utils.get_enum_value(SimulatedPlayer, simulate_player)
+		if player != null:
+			_simulate_player = player
+		else:
+			_simulate_player = SimulatedPlayer.PlayerA
 	var local_user = LocalUser.create_local_user(Utils.get_enum_name(SimulatedPlayer, _simulate_player))
 	$ConnectionController.set_local_user(local_user)
--- Readme.md
+++ Readme.md
@@ -1,1 +1,10 @@
 # Godot RL Agent Play Ground
+
+# Train
+1. install godot_rl_agent
+2. in godot-rl-agents folder, run
+```
+python train.py
+```
+3. in godot editor, open training_group.scene 
+4. click play button
